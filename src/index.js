afd7ea3c2c5a7f0bf1a01af22f6d453c209c3538
dzevakov <dmitryzev@gmail.com>
2020-12-25 02:02:58 +0300

    feat: implement morse decoder

--- src/index.js
+++ src/index.js
@@ -38,9 +38,43 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  var simbolAmount = expr.length / 10;
+  var simbolArray = [];
+  var charWord = 0;
+  var wordArray = [];
+
+  for(var i = 0; i < simbolAmount; i += 1) {
+    simbolArray.push(expr.slice(charWord, charWord + 10));
+
+    if(simbolArray[i].length < 10) {
+      simbolArray[i] = "0" + simbolArray[i];
+    }
+
+    charWord += 10;
+
+    var simbol = "";
+    var charSimbol = 0;
+
+    for(var j = 0; j < 5; j += 1) {
+      if(simbolArray[i].slice(charSimbol, charSimbol + 2) === "10") {
+        simbol += ".";
+      } else if (simbolArray[i].slice(charSimbol, charSimbol + 2) === "11") {
+        simbol += "-";
+      } else if (simbolArray[i].slice(charSimbol, charSimbol + 2) === "**") {
+        simbol = " ";
+        break;
+      }
+      charSimbol += 2;
+    }
+    if(simbol === " ") { 
+      wordArray.push(" ");
+    }
+    wordArray.push(MORSE_TABLE[simbol]);
+    
+  }
+  return wordArray.join('');
 }
 
 module.exports = {
     decode
-};
+}
